7366ad4196ed507443de5a57d1ca6102f0009981
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-05 17:23:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,9 +28,8 @@ end
 
 def return_string_array(array)
   count = 0 
-  new_array = []
   while count < array.length do
-    return new_array.push(array[count].to_s)
+    return array[count].to_s
     count += 1 
   end
 end
