81d038fb0d4c206965b7cfbfaaec55ff78edf0cd
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-05 17:14:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,11 @@
 # Write your methods here
 
 def loop_message_five_times
+  count = 0 
+  while count < 5 do
+    puts message
+    count += 1 
+  end
   
 end
 
@@ -12,4 +17,6 @@ def output_array
   
 end
 
-def 
+def return_string_array
+  
+end
